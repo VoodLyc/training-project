795916b914f07e81894a738ecb1121e1ff057d23
Johan Sebastián Giraldo Rubio <j.sebasgr036@gmail.com>
2023-02-08 10:58:17 -0500

    Refactor CSS class names

--- src/app.js
+++ src/app.js
@@ -2,43 +2,39 @@ let menu = document.querySelector('.menu')
 let arrow = document.querySelector('.arrow')
 let appName = document.querySelector('.menu__name__label')
 let buttonsContainer = document.querySelector('.navbar__buttons-container')
-let buttons = document.querySelectorAll('navbar__button')
 let selectedButton = 'navbar-btn-1'
-
-console.log(selectedButton)
 
 
 const collapse = () => {
     arrow.classList.toggle('arrow--right')
     menu.classList.toggle('menu--collapsed')
-    
+
     appName.classList.toggle('bx')
     appName.classList.toggle('bxs-ghost')
 
-    if(appName.textContent.length > 0) {
+    if (appName.textContent.length > 0)
         appName.textContent = ''
-    }
-    else {
+    else
         appName.textContent = 'Coopang'
-    }
- }
 
- const selectButton = (e) => {
-    if(e.target.classList.contains('navbar__button')) {
+}
+
+const selectButton = (e) => {
+    if (e.target.classList.contains('navbar-button')) {
         if (selectedButton != e.target.id) {
-            e.target.classList.toggle('navbar__button--selected')
-            document.getElementById(selectedButton).classList.toggle('navbar__button--selected')
+            e.target.classList.toggle('navbar-button--selected')
+            document.getElementById(selectedButton).classList.toggle('navbar-button--selected')
             selectedButton = e.target.id
         }
     }
-    else if(e.target.parentNode.classList.contains('navbar__button')) {
+    else if (e.target.parentNode.classList.contains('navbar-button')) {
         if (selectedButton != e.target.parentNode.id) {
-            e.target.parentNode.classList.toggle('navbar__button--selected')
-            document.getElementById(selectedButton).classList.toggle('navbar__button--selected')
+            e.target.parentNode.classList.toggle('navbar-button--selected')
+            document.getElementById(selectedButton).classList.toggle('navbar-button--selected')
             selectedButton = e.target.parentNode.id
         }
     }
- }
+}
 
 arrow.addEventListener('click', collapse)
 buttonsContainer.addEventListener('click', selectButton)
